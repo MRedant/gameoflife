acb271903426461cb1d8ff59f201af3c53e3a3a1
MRedant <mredant@gmail.com>
2018-03-07 11:03:50 +0100

    our try

--- gameoflife-backend/src/main/java/be/switchfully/gameoflife/backend/gol/GameOfLifeController.java
+++ gameoflife-backend/src/main/java/be/switchfully/gameoflife/backend/gol/GameOfLifeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -17,10 +18,54 @@ public class GameOfLifeController {
     private static Logger logger = Logger.getLogger(GameOfLifeController.class);
 
     @PostMapping(value = "/phase")
-    public List<List<Boolean>> nextGeneration(@RequestBody List<List<Boolean>> currentWorld){
+    public List<List<Boolean>> nextGeneration(@RequestBody List<List<Boolean>> currentWorld) {
         logger.info(currentWorld);
-        Collections.shuffle(currentWorld);
-        return currentWorld;
+        // Collections.shuffle(currentWorld);
+        List<List<Boolean>> newWorld = new ArrayList<>(currentWorld);
+        for (int row = 0; row < currentWorld.size(); row++) {
+            for (int col = 0; col < currentWorld.size(); col++) {
+                //boolean currentElement = currentWorld.get(row).get(col);
+                int numberOfNeighbours = getNeighBoursAlive(currentWorld, row, col);
+                if(numberOfNeighbours == 3){
+                    newWorld.get(row).set(col, true);
+                }
+                else if(numberOfNeighbours >= 4 || numberOfNeighbours < 2) {
+                    newWorld.get(row).set(col, false);
+                }
+            }
+        }
+        return newWorld;
+    }
+
+
+    public int getNeighBoursAlive(List<List<Boolean>> currentWorld, int row, int col){
+        int numberOfElements = 0;
+
+        if (row-1>=0 && col-1>=0 && currentWorld.get(row-1).get(col-1)) {
+            numberOfElements++; }
+        if (row-1>=0 && currentWorld.get(row-1).get(col)) {
+            numberOfElements++;
+        }
+        if (row-1>=0 && col+1<currentWorld.size() && currentWorld.get(row-1).get(col+1)) {
+            numberOfElements++;
+        }
+        if (col-1>=0 && currentWorld.get(row).get(col-1)) {
+            numberOfElements++;
+        }
+        if (col+1<currentWorld.size() && currentWorld.get(row).get(col+1)) {
+            numberOfElements++;
+        }
+        if (row+1<currentWorld.size() && col-1>=0 && currentWorld.get(row+1).get(col-1)) {
+            numberOfElements++;
+        }
+        if (row+1<currentWorld.size() && currentWorld.get(row+1).get(col)) {
+            numberOfElements++;
+        }
+        if (row+1<currentWorld.size() && col+1<currentWorld.size() && currentWorld.get(row+1).get(col+1)) {
+            numberOfElements++;
+        }
+
+        return numberOfElements;
     }
 
 }
