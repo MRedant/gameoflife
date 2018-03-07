dfad5ab4edb65e2dfb4b54cd9787e8d21f53220d
MRedant <mredant@gmail.com>
2018-03-07 18:19:02 +0100

    working version (bug fixed)

--- gameoflife-backend/src/main/java/be/switchfully/gameoflife/backend/gol/GameOfLifeController.java
+++ gameoflife-backend/src/main/java/be/switchfully/gameoflife/backend/gol/GameOfLifeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -20,51 +20,42 @@ public class GameOfLifeController {
     @PostMapping(value = "/phase")
     public List<List<Boolean>> nextGeneration(@RequestBody List<List<Boolean>> currentWorld) {
         logger.info(currentWorld);
-        // Collections.shuffle(currentWorld);
+
         List<List<Boolean>> newWorld = new ArrayList<>(currentWorld);
         for (int row = 0; row < currentWorld.size(); row++) {
+            Boolean[] newRow = new Boolean[currentWorld.size()];
+            Arrays.fill(newRow, false);
+            newWorld.add(Arrays.asList(newRow));
+        }
+
+        int numberOfNeighbours;
+        for (int row = 0; row < currentWorld.size(); row++) {
             for (int col = 0; col < currentWorld.size(); col++) {
-                //boolean currentElement = currentWorld.get(row).get(col);
-                int numberOfNeighbours = getNeighBoursAlive(currentWorld, row, col);
-                if(numberOfNeighbours == 3){
+                numberOfNeighbours = getNeighBoursAlive(currentWorld, row, col);
+                if (numberOfNeighbours == 3) {
                     newWorld.get(row).set(col, true);
                 }
-                else if(numberOfNeighbours >= 4 || numberOfNeighbours < 2) {
+                if (numberOfNeighbours > 3 || numberOfNeighbours < 2) {
                     newWorld.get(row).set(col, false);
                 }
+                if (currentWorld.get(row).get(col) && numberOfNeighbours == 2)
+                    newWorld.get(row).set(col, true);
             }
         }
         return newWorld;
     }
 
-
-    public int getNeighBoursAlive(List<List<Boolean>> currentWorld, int row, int col){
+    public int getNeighBoursAlive(List<List<Boolean>> currentWorld, int row, int col) {
         int numberOfElements = 0;
-
-        if (row-1>=0 && col-1>=0 && currentWorld.get(row-1).get(col-1)) {
-            numberOfElements++; }
-        if (row-1>=0 && currentWorld.get(row-1).get(col)) {
+        if (row > 0 && col > 0 && currentWorld.get(row - 1).get(col - 1)) numberOfElements++;
+        if (row > 0 && currentWorld.get(row - 1).get(col)) numberOfElements++;
+        if (row > 0 && col < currentWorld.size() - 1 && currentWorld.get(row - 1).get(col + 1)) numberOfElements++;
+        if (col > 0 && currentWorld.get(row).get(col - 1)) numberOfElements++;
+        if (col < currentWorld.size() - 1 && currentWorld.get(row).get(col + 1)) numberOfElements++;
+        if (row < currentWorld.size() - 1 && col > 0 && currentWorld.get(row + 1).get(col - 1)) numberOfElements++;
+        if (row < currentWorld.size() - 1 && currentWorld.get(row + 1).get(col)) numberOfElements++;
+        if (row < currentWorld.size() - 1 && col < currentWorld.size() - 1 && currentWorld.get(row + 1).get(col + 1))
             numberOfElements++;
-        }
-        if (row-1>=0 && col+1<currentWorld.size() && currentWorld.get(row-1).get(col+1)) {
-            numberOfElements++;
-        }
-        if (col-1>=0 && currentWorld.get(row).get(col-1)) {
-            numberOfElements++;
-        }
-        if (col+1<currentWorld.size() && currentWorld.get(row).get(col+1)) {
-            numberOfElements++;
-        }
-        if (row+1<currentWorld.size() && col-1>=0 && currentWorld.get(row+1).get(col-1)) {
-            numberOfElements++;
-        }
-        if (row+1<currentWorld.size() && currentWorld.get(row+1).get(col)) {
-            numberOfElements++;
-        }
-        if (row+1<currentWorld.size() && col+1<currentWorld.size() && currentWorld.get(row+1).get(col+1)) {
-            numberOfElements++;
-        }
-
         return numberOfElements;
     }
 
